6d11c719dd3d321babef8702262fc5f67373738a
Jason Talon <jason.talon@radix.com.au>
2020-12-09 19:28:49 +0800

    migrations update

--- hasura/migrations/1607511966808_init/up.sql
+++ hasura/migrations/1607511966808_init/up.sql
@@ -104,3 +104,7 @@ ALTER TABLE ONLY public.captures
     ADD CONSTRAINT captures_camera_id_fkey FOREIGN KEY (camera_id) REFERENCES public.cameras(camera_id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.targets
     ADD CONSTRAINT target_camera_id_fkey FOREIGN KEY (camera_id) REFERENCES public.cameras(camera_id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+
+INSERT INTO public.configs (name, value, updated_at) VALUES ('TARGET_PATH', NULL, '2020-12-09 11:07:11.215529+00');
+INSERT INTO public.configs (name, value, updated_at) VALUES ('OBJECT_DETECTION_URI', NULL, '2020-12-09 11:07:17.831801+00');
